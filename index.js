4618616a6ea6d8debbd2b3a2336eb4315c6b33e8
thorough-developer <thorough.developer@gmail.com>
2020-04-24 20:36:01 +0000

    refactored

--- index.js
+++ index.js
@@ -2,61 +2,10 @@ let AsyncLocalStorage = require('async_hooks').AsyncLocalStorage;
 let als;
 
 if (AsyncLocalStorage == null) {
-  const createNamespace = require('cls-hooked').createNamespace;
-  const hyperid = require('hyperid');
-  const generator = hyperid();
-  const id = generator();
-
-  const namespaceIdentifier = hyperid.decode(id).uuid;
-  
-  const _namespace = createNamespace(namespaceIdentifier);
-  als = {};
-  als.get = function(key) {
-    if (_namespace.active) {
-      return _namespace.get(key);
-    }
-    return undefined;
-  };  
-
-  als.set = function(key, value){
-    if (_namespace.active) {
-      _namespace.set(key, value);
-    }
-  }
-  als.runWith = ( defaults = {}, callback) => {
-     _namespace.run(() => {
-       for (let [key, value] of Object.entries(defaults)) {
-        _namespace.set(key, value);
-       }
-       callback();
-     });
-   };
+  als = require('./cls-facade');
 } else {
- als = new class AsyncLocalStorageFacade extends AsyncLocalStorage {
-  constructor() {
-   super();
-  }
-
-  runWith(defaults, callback) {
-    this.run(new Map(Object.entries(defaults)), () => {
-      callback();
-    });
-  }
-
-  set(key, value) {
-    const store = this.getStore();
-
-    if (store != null) {
-     store.set(key, value);
-    }
-  }
-
-  get(key) {
-    const store = this.getStore();
-
-    return store != null ? store.get(key) : undefined;
-  }
- }();
+ console.log('als facade');
+ als = require('./als-facade');
 }
 
 module.exports = als;
