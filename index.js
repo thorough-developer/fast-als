00bac987ea5eee0ef6736c7cd7ace19ec58bcaec
thorough-developer <thorough.developer@gmail.com>
2020-04-26 14:19:57 +0000

    need this for eslint

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const { AsyncLocalStorage } = require('async_hooks');
+const { AsyncLocalStorage } = require("async_hooks");
 
 /**
  * Asynchronous Local Storage wrapper that will run regardless of Node version.
- * 
+ *
  * @module fastAls
  */
 let fastAls = {};
@@ -12,29 +12,29 @@ let fastAls = {};
 /**
  * The start of creating an asynchronous local storage context. Once this method is called, a new context is created
  * where get and set calls will set and return values as expected.
- * 
+ *
  * @memberof fastAls
  * @param {object} defaults - Sets the default values. A convenience so that you don't have to check to see if a value is there and set it to something else.
  * @param {fastAls~runWithCallback} callback - This is the code to be executed first within the new context that is created
- * 
+ *
  * @example
  * const fastAls = require('fast-als');
- * 
+ *
  * function firstCallInScope() {
  *   // override user
  *   fastAls.set('user', { id: 'overwrittenUser'});
  * }
- * 
+ *
  * function secondCallInScope() {
  *  // will print the user set in firstCallInScope
  *  console.log(fastAls.get('user'));
  * }
- * 
+ *
  * fastAls.runWith({ user: { id: 'someUser' } }, () => {
  *   firstCallInScope();
  *   secondCallInScope();
  * });
-*/
+ */
 fastAls.runWith;
 
 /**
@@ -43,29 +43,29 @@ fastAls.runWith;
  * @memberof fastAls
  * @param {string} key - the key to store the variable by
  * @param {any} value - the value to store under the key for lookup later on.
- * 
+ *
  * @example
- * 
+ *
  * const fastAls = require('fast-als');
- * 
+ *
  * function callInScope() {
  *   // override user
  *   fastAls.set('user', { id: 'overwrittenUser'});
  * }
- * 
+ *
  * fastAls.runWith({ user: { id: 'someUser' } }, () => {
  *   callInScope();
  * });
- * 
+ *
  * @example
- * 
+ *
  * const fastAls = require('fast-als');
- * 
+ *
  * function callOutOfScope() {
  *   // this never gets set
  *   fastAls.set('user', { id: 'overwrittenUser'});
  * }
- * 
+ *
  * // calling this won't store the variable under the key
  * callOutOfScope();
  */
@@ -74,51 +74,51 @@ fastAls.set;
 /**
  * Gets a variable previously set within a running context (started by runWith).
  * If this is called outside of a running context, it will not retrieve the value.
- * 
+ *
  * @memberof fastAls
  * @param {string} key - the key to retrieve the stored value
- * 
+ *
  * @example
- * 
+ *
  * const fastAls = require('fast-als');
- * 
+ *
  * function callInScope() {
  *   // prints default user
  *   console.log(fastAls.get('user'));
  * }
- * 
+ *
  * fastAls.runWith({ user: { id: 'someUser' } }, () => {
  *   callInScope();
  * });
- * 
+ *
  * @example
- * 
+ *
  * const fastAls = require('fast-als');
- * 
+ *
  * function callInScope() {
  *   // prints default user
  *   console.log(fastAls.get('user'));
  * }
- * 
+ *
  * fastAls.runWith({ user: { id: 'someUser' } }, () => {
  *   callInScope();
  * });
- * 
+ *
  * // calling this will return undefined
  * callInScope();
  */
 fastAls.get;
 
 if (AsyncLocalStorage == null) {
-    fastAls = require('./cls-facade');
+  fastAls = require("./cls-facade");
 } else {
-    fastAls = require('./als-facade');
+  fastAls = require("./als-facade");
 }
 
 module.exports = fastAls;
 
 /**
  * The code to be executed after the new asynchronous local storage context is created.
- * 
+ *
  * @callback fastAls~runWithCallback
  */
