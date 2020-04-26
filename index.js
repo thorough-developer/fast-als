fe31d4f02eb39093d6b48fe5bee914aa4a56ef54
thorough-developer <thorough.developer@gmail.com>
2020-04-26 12:08:19 +0000

    starting of jsdoc

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 const { AsyncLocalStorage } = require('async_hooks');
 
+/**
+ * 
+ * @module fastAls
+ */
 let fastAls;
+
+// these are just for documenting purposes
+
+/**
+ * @param {object} defaults - Sets the default values. A convenience so that you don't have to check to see if a value is there and set it to something else.
+ * @param {function} callback - This is the code to be executed first within the new context that is created
+ * 
+ * @example
+ * const fastAls = require('fast-als');
+ * 
+ * function firstCallInScope() {
+ *   // override user
+ *   fastAls.set('user', { id: 'overwrittenUser'});
+ * }
+ * 
+ * function secondCallInScope() {
+ *  // will print the user set in firstCallInScope
+ *  console.log(fastAls.get('user'));
+ * }
+ * 
+ * fastAls.runWith({ user: { id: 'someUser' } }, () => {
+ *   firstCallInScope();
+ *   secondCallInScope();
+ * })
+*/
+fastAls.runWith;
+
+fastAls.set;
+
+fastAls.get;
 
 if (AsyncLocalStorage == null) {
     fastAls = require('./cls-facade');
